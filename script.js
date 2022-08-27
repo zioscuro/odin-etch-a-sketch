90ffd9b338141caaf0d67a68cc3ac436dfbf0fc9
zioscuro <lore.rang@gmail.com>
2022-08-28 01:16:12 +0200

    Add pointer color control option

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 // Main grid container DOM element    
 const container = document.querySelector('#container');
 // Grid controls DOM elements
-const gridSizeInput = document.querySelector('#grid-size-input');
-const gridSizeBtn = document.querySelector('#btn-grid-size');
+const gridSizeInput = document.querySelector('#gridSize-input');
+const gridSizeBtn = document.querySelector('#btn-gridSize');
+// Color controls DOM elements
+const selectColorInput = document.querySelector('#selectColor');
+const selectColorBtn = document.querySelector('#btn-selectColor');
 
 // Initial grid setup
 let gridSize = 16;
 createGrid(gridSize);
 
-// Add event listener to magange a grid size change
+//  Initial cell setup
+let cellColor = '#000000';
+
+// CONTROLS EVENT LISTENERS
+// grid size change button 
 gridSizeBtn.addEventListener('click', ()=>{
     // first remove existing grid
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    // get new grid size from input and create grid
+    // get new grid size from input and create new grid
     gridSize = gridSizeInput.value;
     createGrid(gridSize);
-})
+});
+// change color button
+selectColorBtn.addEventListener('click', ()=>{
+    // get new color from input and update cellColor variable
+    cellColor = selectColorInput.value;
+    console.log(cellColor);
+});
 
 // FUNCTIONS
 // create grid in the container based on a grid size parameter
@@ -25,12 +38,12 @@ function createGrid(size) {
     for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-    
+        
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.addEventListener('mouseover', (e)=>{
-                e.target.classList.add('cellActive');
+            cell.addEventListener('mouseover', ()=>{
+                cell.style.backgroundColor = cellColor;
             })
             row.appendChild(cell);
         }
