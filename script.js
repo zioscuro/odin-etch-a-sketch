2b051fd64fb31bf81a058fd00e73b9a0a878be14
zioscuro <lore.rang@gmail.com>
2022-08-28 00:03:38 +0200

    Add grid size selector

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-const container = document.querySelector('#container')
+// Main grid container DOM element    
+const container = document.querySelector('#container');
+// Grid controls DOM elements
+const gridSizeInput = document.querySelector('#grid-size-input');
+const gridSizeBtn = document.querySelector('#btn-grid-size');
 
+// Initial grid setup
 let gridSize = 16;
+createGrid(gridSize);
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-
-    for (let j = 0; j < gridSize; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.addEventListener('mouseover', (e)=>{
-            e.target.classList.add('cellActive');
-        })
-        row.appendChild(cell);
+// Add event listener to magange a grid size change
+gridSizeBtn.addEventListener('click', ()=>{
+    // first remove existing grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(row);
+    // get new grid size from input and create grid
+    gridSize = gridSizeInput.value;
+    createGrid(gridSize);
+})
+
+// FUNCTIONS
+// create grid in the container based on a grid size parameter
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+    
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener('mouseover', (e)=>{
+                e.target.classList.add('cellActive');
+            })
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
 }
